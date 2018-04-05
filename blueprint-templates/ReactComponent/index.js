e2647f01d813480c329e08a54149298aecfc702a
Igor Marcossi <igormarcossi@gmail.com>
2018-04-05 01:31:47 -0300

    Adding stuff

---
+++ blueprint-templates/ReactComponent/index.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import './styles.scss';
+
+const {{ pascalCase name }} = () => {
+   return (
+      <div>
+      </div>
+   )
+}
+export default {{ pascalCase name }};
+
+
+
+
+// export default class {{pascalCase name}} extends React.Component {
+//    constructor(props) {
+//       super(props);
+//       this.state = {
+//          value: null,
+//       };
+//    }
+// 
+//    componentDidMount() {
+//    }
+// 
+//    componentWillUnmount() {
+//    }
+// 
+//    render() {
+//       return (
+//          <div>
+// 
+//          </div>
+//       );
+//    }
+// }
---
+++ blueprint-templates/StatefulComponent/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './styles.scss';
+
+export default class {{pascalCase name}} extends React.Component {
+   constructor(props) {
+      super(props);
+      this.state = {
+         value: null,
+      };
+   }
+
+   componentDidMount() {
+   }
+
+   componentWillUnmount() {
+   }
+
+   render() {
+      return (
+         <div>
+
+         </div>
+      );
+   }
+}
