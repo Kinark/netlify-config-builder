3016a35bf6dc01edd2ae1d1118aa7958759c8a39
Kinark <igormarcossi@gmail.com>
2018-06-06 01:57:28 -0300

    Added global styles

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 import App from '~/App';
+import './styles.global.scss'
 // import { BrowserRouter, Route } from 'react-router-dom';
 
 const root = document.getElementById('root')
