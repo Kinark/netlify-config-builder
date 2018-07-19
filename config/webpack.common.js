8ce067b382207256f1e0e1c8791ebdf7261ffef4
Kinark <igormarcossi@gmail.com>
2018-07-19 15:59:46 -0300

    Added font handler for webpack

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -10,9 +10,18 @@ const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
 
 const imgLoader = {
    loader: 'url-loader',
-   options: { 
+   options: {
       limit: 8000,
       name: 'static/media/[hash]-[name].[ext]'
+   }
+}
+
+const fontLoader = {
+   loader: 'url-loader',
+   options: {
+      limit: 50000,
+      mimetype: 'application/font-woff',
+      name: 'static/fonts/[name].[ext]',
    }
 }
 
@@ -23,8 +32,9 @@ module.exports = {
    },
    module: {
       rules: [
-         { test: /\.(js|jsx)$/, use: [{loader: 'babel-loader', options: {cacheDirectory: true}}], exclude: /node_modules/ },
-         { test: /\.(png|jp(e*)g|svg|ico)$/, use: [ imgLoader ] },
+         { test: /\.(js|jsx)$/, use: [{ loader: 'babel-loader', options: { cacheDirectory: true } }], exclude: /node_modules/ },
+         { test: /\.(png|jp(e*)g|svg|ico)$/, use: [imgLoader] },
+         { test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/, use: [fontLoader] },
       ],
    },
    plugins: [HtmlWebpackPluginConfig],
