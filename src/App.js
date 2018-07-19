b566c625c0cd316c950a8b3269465ebc4831e781
Kinark <igormarcossi@gmail.com>
2018-07-19 14:37:25 -0300

    Code optimization

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import { hot } from 'react-hot-loader'
 import { Metas } from '~/components/Metas';
 import Favicon from '~/components/Favicon';
 
-const title = "Sample Website";
-const description = "A sample website.";
+const title = 'Sample Website';
+const description = 'A sample website.';
 // const cover = "";
 
 const App = () => (
