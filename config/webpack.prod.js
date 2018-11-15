146fb8a348c47cedfa046699b5fa5fbddf866022
Kinark <igormarcossi@gmail.com>
2018-11-15 18:53:32 -0200

    Added support for .global css files

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -48,8 +48,10 @@ module.exports = merge(common, {
    },
    module: {
       rules: [
-         { test: /\.css$/, use: [MiniCssExtractPlugin.loader, cssLoader, postCssLoader] },
-         { test: /\.scss$/, use: [MiniCssExtractPlugin.loader, cssLoader, postCssLoader, 'sass-loader'] },
+         { test: /\.global\.css$/, use: [MiniCssExtractPlugin.loader, 'css-loader', postCssLoader] },
+         { test: /\.global\.(scss|sass)$/, use: [MiniCssExtractPlugin.loader, 'css-loader', postCssLoader, 'sass-loader'] },
+         { test: /^((?!\.global).)*\.css$/, use: [MiniCssExtractPlugin.loader, cssLoader, postCssLoader] },
+         { test: /^((?!\.global).)*\.(scss|sass)$/, use: [MiniCssExtractPlugin.loader, cssLoader, postCssLoader, 'sass-loader'] },
       ],
    },
    plugins: [
