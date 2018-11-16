253af796775918e9fb32422d444799c1bb5a7134
Kinark <igormarcossi@gmail.com>
2018-11-16 15:49:43 -0200

    Refactored Metas component

--- src/components/Metas/index.js
+++ src/components/Metas/index.js
@@ -1,94 +1,86 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {Helmet} from "react-helmet";
+import { Helmet } from 'react-helmet';
 
-export const Cover = (props) => {
-   const cover = props.children;
-   return (
-      <Helmet>
-         <meta property="og:image" content={cover} />
-         <meta name="twitter:image" content={cover} />
-      </Helmet>
-   )
-}
+export const Cover = ({ children: cover }) => (
+   <Helmet>
+      <meta property="og:image" content={cover} />
+      <meta name="twitter:image" content={cover} />
+   </Helmet>
+)
 
 Cover.propTypes = {
-   children: PropTypes.object.isRequired
+   children: PropTypes.shape({}).isRequired,
 }
 
-export const Description = (props) => {
-   const desc = props.children;
-   return (
-      <Helmet>
-         <meta name="description" content={desc} />
-         <meta property="og:description" content={desc} />
-         <meta name="twitter:description" content={desc} />
-      </Helmet>
-   )
-}
+export const Description = ({ children: desc }) => (
+   <Helmet>
+      <meta name="description" content={desc} />
+      <meta property="og:description" content={desc} />
+      <meta name="twitter:description" content={desc} />
+   </Helmet>
+)
 
 Description.propTypes = {
    children: PropTypes.string.isRequired
 }
 
-export const Title = (props) => {
-   const title = props.children;
-   return (
-      <Helmet>
-         <title>{title}</title>
-         <meta property="og:title" content={title} />
-         <meta property="og:site_name" content={title} />
-         <meta name="twitter:title" content={title} />
-         <meta name="apple-mobile-web-app-title" content={title} />
-         <meta name="application-name" content={title} />
-      </Helmet>
-   )
-}
+export const Title = ({ children: title }) => (
+   <Helmet>
+      <title>{title}</title>
+      <meta property="og:title" content={title} />
+      <meta property="og:site_name" content={title} />
+      <meta name="twitter:title" content={title} />
+      <meta name="apple-mobile-web-app-title" content={title} />
+      <meta name="application-name" content={title} />
+   </Helmet>
+)
 
 Title.propTypes = {
    children: PropTypes.string.isRequired
 }
 
-export const Metas = (props) => {
-   const title = props.title ? props.title : '';
-   const description = props.description ? props.description : '';
-   const cover = props.cover ? props.cover : '';
-   const url = props.url ? props.url : '';
-   const color = props.color ? props.color : '';
-   return (
-      <Helmet>
-         <title>{title}</title>
-         
-         <meta name="description" content={description} />
-   
-         <meta property="og:title" content={title} />
-         <meta property="og:site_name" content={title} />
-         <meta property="og:description" content={description} />
-         <meta property="og:url" content={url} />
-         <meta property="og:image" content={cover} />
-   
-         <meta name="twitter:card" content="summary" />
-         <meta name="twitter:url" content={url} />
-         <meta name="twitter:title" content={title} />
-         <meta name="twitter:description" content={description} />
-         <meta name="twitter:image" content={cover} />
-   
-         <meta name="mobile-web-app-capable" content="yes" />
-         <meta name="apple-mobile-web-app-capable" content="yes" />
-         <meta name="apple-mobile-web-app-title" content={title} />
-         <meta name="application-name" content={title} />
-         
-         <meta name="msapplication-TileColor" content={color} />
-         <meta name="theme-color" content={color} />
-         <meta name="apple-mobile-web-app-status-bar-style" content={color} />
-      </Helmet>
-   )
-}
+export const Metas = ({ title, description, cover, url, color }) => (
+   <Helmet>
+      {title && <title>{title}</title>}
+
+      {title && <meta name="description" content={description} />}
+
+      {title && <meta property="og:title" content={title} />}
+      {title && <meta property="og:site_name" content={title} />}
+      {description && <meta property="og:description" content={description} />}
+      {url && <meta property="og:url" content={url} />}
+      {cover && <meta property="og:image" content={cover} />}
+
+      <meta name="twitter:card" content="summary" />
+      {url && <meta name="twitter:url" content={url} />}
+      {title && <meta name="twitter:title" content={title} />}
+      {description && <meta name="twitter:description" content={description} />}
+      {cover && <meta name="twitter:image" content={cover} />}
+
+      <meta name="mobile-web-app-capable" content="yes" />
+      <meta name="apple-mobile-web-app-capable" content="yes" />
+      {title && <meta name="apple-mobile-web-app-title" content={title} />}
+      {title && <meta name="application-name" content={title} />}
+
+      {color && <meta name="msapplication-TileColor" content={color} />}
+      {color && <meta name="theme-color" content={color} />}
+      {color && <meta name="apple-mobile-web-app-status-bar-style" content={color} />}
+   </Helmet>
+)
 
 Metas.propTypes = {
    title: PropTypes.string,
    description: PropTypes.string,
-   cover: PropTypes.object,
+   cover: PropTypes.shape({}),
    url: PropTypes.string,
    color: PropTypes.string,
+}
+
+Metas.defaultProps = {
+   title: '',
+   description: '',
+   cover: {},
+   url: '',
+   color: '',
 }
