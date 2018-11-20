5967922dfb82a4163b9639246ade02d9cb8d4318
Kinark <igormarcossi@gmail.com>
2018-11-20 05:51:48 -0200

    Remove useless stuff from stateless blueprint component

--- blueprint-templates/StatelessComponent/index.js
+++ blueprint-templates/StatelessComponent/index.js
@@ -1,10 +1,7 @@
 import React from 'react';
-import classNames from 'classnames/bind';
 import PropTypes from 'prop-types';
 
 import styles from './styles.scss';
-
-let cx = classNames.bind(styles);
 
 const {{pascalCase name}} = props => (
    <div>
