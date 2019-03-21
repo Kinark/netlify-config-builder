052f7cd7793b160e7292072119c4127de6c36923
Kinark <igormarcossi@gmail.com>
2019-03-21 20:13:24 -0300

    Updated blueprints

--- blueprint-templates/StatefulComponent/index.js
+++ blueprint-templates/StatefulComponent/index.js
@@ -1,10 +1,6 @@
 import React from 'react';
-import classNames from 'classnames/bind';
 import PropTypes from 'prop-types';
-
-import styles from './styles.scss';
-
-let cx = classNames.bind(styles);
+import styled from 'styled-components';
 
 export default class {{ pascalCase name }} extends React.Component {
    static propTypes = {
--- blueprint-templates/StatelessComponent/index.js
+++ blueprint-templates/StatelessComponent/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
-import styles from './styles.scss';
+import styled from 'styled-components';
 
 const {{pascalCase name}} = props => (
    <div>
