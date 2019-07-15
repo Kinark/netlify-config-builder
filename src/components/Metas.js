1c049f2bfa9a1b3b5cb1519b4b7d7d1cbbf4534a
Kinark <igormarcossi@gmail.com>
2019-07-15 11:32:09 -0300

    Fixed Metas [object Object]

--- src/components/Metas.js
+++ src/components/Metas.js
@@ -42,45 +42,45 @@ Title.propTypes = {
 
 export const Metas = ({ title, description, cover, url, color }) => (
    <Helmet>
-      {title && <title>{title}</title>}
+      {!!title && <title>{title}</title>}
 
-      {title && <meta name="description" content={description} />}
+      {!!title && <meta name="description" content={description} />}
 
-      {title && <meta property="og:title" content={title} />}
-      {title && <meta property="og:site_name" content={title} />}
-      {description && <meta property="og:description" content={description} />}
-      {url && <meta property="og:url" content={url} />}
-      {cover && <meta property="og:image" content={cover} />}
+      {!!title && <meta property="og:title" content={title} />}
+      {!!title && <meta property="og:site_name" content={title} />}
+      {!!description && <meta property="og:description" content={description} />}
+      {!!url && <meta property="og:url" content={url} />}
+      {!!cover && <meta property="og:image" content={cover} />}
 
       <meta name="twitter:card" content="summary" />
-      {url && <meta name="twitter:url" content={url} />}
-      {title && <meta name="twitter:title" content={title} />}
-      {description && <meta name="twitter:description" content={description} />}
-      {cover && <meta name="twitter:image" content={cover} />}
+      {!!url && <meta name="twitter:url" content={url} />}
+      {!!title && <meta name="twitter:title" content={title} />}
+      {!!description && <meta name="twitter:description" content={description} />}
+      {!!cover && <meta name="twitter:image" content={cover} />}
 
       <meta name="mobile-web-app-capable" content="yes" />
       <meta name="apple-mobile-web-app-capable" content="yes" />
-      {title && <meta name="apple-mobile-web-app-title" content={title} />}
-      {title && <meta name="application-name" content={title} />}
+      {!!title && <meta name="apple-mobile-web-app-title" content={title} />}
+      {!!title && <meta name="application-name" content={title} />}
 
-      {color && <meta name="msapplication-TileColor" content={color} />}
-      {color && <meta name="theme-color" content={color} />}
-      {color && <meta name="apple-mobile-web-app-status-bar-style" content={color} />}
+      {!!color && <meta name="msapplication-TileColor" content={color} />}
+      {!!color && <meta name="theme-color" content={color} />}
+      {!!color && <meta name="apple-mobile-web-app-status-bar-style" content={color} />}
    </Helmet>
 )
 
 Metas.propTypes = {
    title: PropTypes.string,
    description: PropTypes.string,
-   cover: PropTypes.shape({}),
+   cover: PropTypes.string,
    url: PropTypes.string,
    color: PropTypes.string,
 }
 
 Metas.defaultProps = {
-   title: '',
-   description: '',
-   cover: {},
-   url: '',
-   color: '',
+   title: undefined,
+   description: undefined,
+   cover: undefined,
+   url: undefined,
+   color: undefined,
 }
