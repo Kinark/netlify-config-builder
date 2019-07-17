f565fb051cb479161b826387709c63d309cb139a
Kinark <igormarcossi@gmail.com>
2019-07-17 12:17:49 -0300

    Fixed wrong hot accept

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import { render } from 'react-dom';
-import App from '~/App';
+import React from 'react'
+import { render } from 'react-dom'
+import App from '~/App'
 import './styles.global.scss'
-// import { BrowserRouter, Route } from 'react-router-dom';
 
 const root = document.getElementById('root')
 
-// render(<BrowserRouter><Route component={App} /></BrowserRouter>, root);
-render(<App />, root);
+render(<App />, root)
 
-if (module.hot) {
-   module.hot.accept()
-}
+module.hot.accept('./App.js', () => {
+   // eslint-disable-next-line global-require
+   const NextApp = require('./App.js').default
+   render(<NextApp />, root)
+})
