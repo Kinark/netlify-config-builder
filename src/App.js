e78575f54bb01114f3d63618c096420817605c62
Kinark <igormarcossi@gmail.com>
2019-07-17 12:20:12 -0300

    Imported hot before react

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React from 'react';
 import { hot } from 'react-hot-loader/root'
+import React from 'react';
 import { BrowserRouter } from 'react-router-dom'
 
 import { Metas } from '~/components/Metas';
