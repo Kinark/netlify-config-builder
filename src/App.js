4bf7e212072972733c569a5d9084fbfe84204df7
Kinark <igormarcossi@gmail.com>
2019-12-09 00:25:43 -0300

    Added global style

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { hot } from 'react-hot-loader/root'
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom'
+import { createGlobalStyle } from 'styled-components'
 
 import { Metas } from '~/components/Metas';
 import Favicon from '~/components/Favicon';
@@ -11,7 +12,17 @@ const App = () => (
       <Metas title="Sample Website" description="" />
       <Favicon />
       <AppRoutes />
+      <GlobalStyle />
    </BrowserRouter>
 )
 
 export default hot(App)
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    background-color: #F6F7FB;
+    color: #1A213A;
+    font-family: 'Lato', sans-serif;
+    line-height: normal;
+  }
+`
