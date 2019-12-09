b5289187a7586bea86ccbf3aa397e9320843709b
Kinark <igormarcossi@gmail.com>
2019-12-09 18:06:16 -0300

    New Button style

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,11 +1,17 @@
 import styled from 'styled-components';
 
 const Button = styled.button`
-   border: solid 2px gray;
    border-radius: 5px;
-   padding: 5px 15px;
-   font-size: 16px;
+   padding: 7px 15px;
+   font-size: 14px;
    font-family: inherit;
+   background-color: #6a76a2;
+   color: white;
+   transition: background-color 300ms ease-out;
+   margin: 5px;
+   &:hover {
+      background-color: #5b6690;
+   }
 `
 
 export default Button
