cdf6b4ef4e61dec6d317f130a22106c3c975b816
Kinark <igormarcossi@gmail.com>
2019-12-09 21:18:46 -0300

    Created Select component

---
+++ src/components/Select.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+const Select = styled.select`
+   background: none;
+   border: none;
+   font-size: 14px;
+   margin-bottom: 10px;
+   padding: 0;
+`
+
+export default Select
