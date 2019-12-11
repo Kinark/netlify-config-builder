463ea4d50e84a610c04bb69e7ae0c28513d77ef6
Kinark <igormarcossi@gmail.com>
2019-12-10 23:32:56 -0300

    Added fork me on github

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, { useEffect } from 'react'
 import { BrowserRouter } from 'react-router-dom'
 import { createGlobalStyle } from 'styled-components'
 import ReactGA from 'react-ga'
+import GitHubForkRibbon from 'react-github-fork-ribbon'
 
 import { Metas } from '~/components/Metas'
 import Favicon from '~/components/Favicon'
@@ -11,10 +12,17 @@ import AppRoutes from '~/components/AppRoutes'
 ReactGA.initialize('UA-128647381-4')
 ReactGA.pageview(window.location.pathname + window.location.search)
 
+const ForkMe = () => (
+   <GitHubForkRibbon href="https://github.com/Kinark/netlify-config-builder" target="_blank" position="right">
+      Fork me on GitHub
+   </GitHubForkRibbon>
+)
+
 const App = () => (
    <BrowserRouter>
       <Metas title="Netlify Config Builder" description="Stop messing around with YML" />
       <Favicon />
+      <ForkMe />
       <AppRoutes />
       <GlobalStyle />
    </BrowserRouter>
